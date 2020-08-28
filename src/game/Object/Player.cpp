8521cf0e127960b2b536d5528d051d4ecdc39eb5
celguar <celguar@gmail.com>
2020-08-28 12:34:37 +0300

    Disable quest end scripts on randomisation

--- src/game/Object/Player.cpp
+++ src/game/Object/Player.cpp
@@ -15161,6 +15161,9 @@ void Player::RewardQuest(Quest const* pQuest, uint32 reward, Object* questGiver,
 
     bool handled = false;
 
+#ifdef ENABLE_PLAYERBOTS
+    if (this != questGiver){
+#endif
     switch (questGiver->GetTypeId())
     {
         case TYPEID_UNIT:
@@ -15173,6 +15176,7 @@ void Player::RewardQuest(Quest const* pQuest, uint32 reward, Object* questGiver,
 
     // don't start quest script during randomization
 #ifdef ENABLE_PLAYERBOTS
+}
     if (this != questGiver && !handled && pQuest->GetQuestCompleteScript() != 0)
 #else
     if (!handled && pQuest->GetQuestCompleteScript() != 0)
